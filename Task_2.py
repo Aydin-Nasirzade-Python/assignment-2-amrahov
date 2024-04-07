becc21224126d41e9cba3e3e78e035e348f7b916
amrahov <164860131+amrahov@users.noreply.github.com>
2024-04-07 23:39:13 +0400

    Update Task_2.py

--- Task_2.py
+++ Task_2.py
@@ -9,7 +9,7 @@ def main():
       print(f"{month} {day} is in Summer")
   elif (month=="September" and day>=22) or month=="October" or month=="November" or (month=="December" and day<21):
       print(f"{month} {day} is in Fall")
-  elif (month=="December" and day>=21) or month=="January" or month=="Febrary" or(month=="March" and day<20):
+  elif (month=="December" and day>=21) or month=="January" or month=="February" or(month=="March" and day<20):
       print(f"{month} {day} is in Winter")
   pass
 
